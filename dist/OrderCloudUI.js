24eaa3633ad2e4a7b6a6db3b50a5f675fea43344
Robert <rwatt@four51.com>
2015-05-28 09:45:45 -0500

    Added window and backdrop templates into ocModal

--- dist/OrderCloudUI.js
+++ dist/OrderCloudUI.js
@@ -3460,7 +3460,8 @@ angular.module("template/datepicker/year.html", []).run(["$templateCache", funct
       "</table>\n" +
       "");
 }]);
-angular.module('orderCloud.modal', [])
+angular.module("orderCloud.modal.tpls", ["template/modal/backdrop.html","template/modal/window.html"]);
+angular.module('orderCloud.modal', ['orderCloud.modal.tpls'])
 
 /**
  * A helper, internal data structure that acts as a map but also allows getting / removing
@@ -3936,6 +3937,18 @@ angular.module('orderCloud.modal', [])
 		return $modalProvider;
 	});
 
+angular.module("template/modal/backdrop.html", []).run(["$templateCache", function($templateCache) {
+	$templateCache.put("template/modal/backdrop.html",
+		"<div class=\"modal-backdrop\" modal-animation-class=\"fade\" ng-class=\"{in: animate}\" ng-style=\"{'z-index': 1040 + (index && 1 || 0) + index*10}\"></div>"
+	);
+}]);
+
+angular.module("template/modal/window.html", []).run(["$templateCache", function($templateCache) {
+	$templateCache.put("template/modal/window.html",
+		"<div modal-render=\"{{$isRendered}}\" tabindex=\"-1\" role=\"dialog\" class=\"modal\" modal-animation-class=\"fade\" ng-class=\"{in: animate}\" ng-style=\"{'z-index': 1050 + index*10, display: 'block'}\" ng-click=\"close($event)\">\n" +
+		"<div class=\"modal-dialog\" ng-class=\"size ? 'modal-' + size : ''\"><div class=\"modal-content\" modal-transclude></div></div></div>"
+	);
+}]);
 angular.module('orderCloud.validate', [])
 	.directive('ocValidate', ocValidate)
 	.controller( 'ocValidateCtrl', ocValidateCtrl)
